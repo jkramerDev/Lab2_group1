e2911c51298f779bd308c97a24942ebe17fd46ec
Cameron Palmer <cameron.palmer@student.fairfield.edu>
2024-02-07 16:45:09 -0500

    first commit

--- linkedList/LinkedListImpl.java
+++ linkedList/LinkedListImpl.java
@@ -2,37 +2,95 @@ package linkedList;
 
 public class LinkedListImpl implements LinkedList {
 
+	ListItem head;
+	
+	public LinkedListImpl() {
+		
+	}
+	
 	@Override
-	public Boolean isItemInList(String thisItem) {
-		// TODO Auto-generated method stub
-		return null;
+	public Boolean isItemInList(String item) {
+		ListItem current = head;
+		
+		if (current == null) {
+			return false;
+		}
+		
+		while (current != null) {
+			if (current.data == item) {
+				return true;
+			}
+			current = current.next;
+		}
+		return false;
 	}
 
 	@Override
-	public Boolean addItem(String thisItem) {
+	public void add(String item) {
 		
-		
-		System.out.println("hello from addItem in LinkedListImpl - the item passed in: " + thisItem);
-	
-		return null;
+		if (head == null) {
+			head = new ListItem(item);
+			return;
+		}
+		else {
+			ListItem current = head;
+			while (current.next != null) {
+				current = current.next;
+			}
+			current.next = new ListItem(item);
+		}
 	}
 
 	@Override
 	public Integer itemCount() {
-		// TODO Auto-generated method stub
-		return null;
+		int count = 0;
+		ListItem current = head;
+		if (head == null) {
+			return count;
+		}
+		count++;
+		while (current.next != null) {
+			current = current.next;
+			count++;
+		}
+		return count;
 	}
 
 	@Override
 	public void listItems() {
-		// TODO Auto-generated method stub
-		
+		ListItem current = head;
+		System.out.print("[");
+		while (current != null) {
+			System.out.print(current.data);
+			current = current.next;
+			if (current != null) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println("]");
 	}
 
 	@Override
-	public Boolean deleteItem(String thisItem) {
-		// TODO Auto-generated method stub
-		return null;
+	public void delete(String item) {
+		if (isItemInList(item) == false) {
+			System.out.println("Could not find selected item to delete: ('" + item + "')");
+			return;
+		}
+		if (head == null)
+			return;
+		if (head.data == item) {
+			head = head.next;
+			return;
+		}
+		ListItem current = head;
+		while (current.next != null) {
+			if (current.next.data == item) {
+				current.next = current.next.next;
+				return;
+			}
+			current = current.next;
+		}
+		
 	}
 
 	@Override
