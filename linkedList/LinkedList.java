dfd0f9bc91643c074fcf6bf770258121b25b71aa
Cameron Palmer <cameron.palmer@student.fairfield.edu>
2024-02-07 16:57:57 -0500

    first commit

--- linkedList/LinkedList.java
+++ linkedList/LinkedList.java
@@ -3,13 +3,13 @@ package linkedList;
  
 
 public interface LinkedList {
-	public Boolean isItemInList(String thisItem);
+	public Boolean isItemInList(String Item);
 		// true if it is, false if not
-	public Boolean addItem(String thisItem);
+	public void add(String Item);
 		// true if added, false if it was already there, or an error
 	public Integer itemCount();
 	public void listItems(); 
-	public Boolean deleteItem(String thisItem);
+	public void delete(String Item);
 		// true if deleted, false if not there or error
 	public Boolean insertBefore(String newItem, String itemToInsertBefore);
 	public Boolean insertAfter(String newItem, String itemToInsertAfter);
