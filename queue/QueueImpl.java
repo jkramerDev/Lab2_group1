92e7b785b3bc904f559260e6fa83ef7da9a8df6e
Cameron Palmer <cameron.palmer@student.fairfield.edu>
2024-02-14 16:00:03 -0500

    first commit

--- queue/QueueImpl.java
+++ queue/QueueImpl.java
@@ -2,22 +2,46 @@ package queue;
 
 public class QueueImpl implements Queue {
 
+	int LENGTH = 5;
+	String[] arr = new String[LENGTH];
+	int size = 0;
+
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
+		if (arr[LENGTH - 1] != null) {
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
+		if (arr[0] == null) {
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public void enQueue(String element) {
-		// TODO Auto-generated method stub
-		
+
+		if (isFull()) {
+			System.err.println("sorry, full queue");
+		} else if (isEmpty()) {
+			arr[0] = element;
+			size++;
+		} else {
+			
+			//everybody shift one to the 'right'
+			for (int i = size; i > 0; i--) {
+				arr[i] = arr[i - 1];
+			}
+			
+			//last one on 
+			arr[0] = element;
+			size++;
+		}
+
 	}
 
 	@Override
