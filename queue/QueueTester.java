a0afad654b5f0ec1601f84fc69c86b1c3cac78ed
Cameron Palmer <cameron.palmer@student.fairfield.edu>
2024-02-14 16:00:41 -0500

    first commit

--- queue/QueueTester.java
+++ queue/QueueTester.java
@@ -4,6 +4,11 @@ public class QueueTester {
 
 	public static void main(String[] args) {
 
+		Queue q = new QueueImpl();
+		q.enQueue("thing 1");
+		q.enQueue("thing 2");
+		q.enQueue("thing 3");
+		
 	}
 
 }
