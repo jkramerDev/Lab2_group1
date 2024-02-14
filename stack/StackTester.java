b1e410658d72a5a2f98e7e57970e96e8d6de3fc0
Cameron Palmer <cameron.palmer@student.fairfield.edu>
2024-02-14 16:02:39 -0500

    first commit

--- stack/StackTester.java
+++ stack/StackTester.java
@@ -1,16 +1,18 @@
 package stack;
 
 public class StackTester {
-
+	
 	public static void main(String[] args) {
 
 		Stack stackTester = new StackImpl();
-		stackTester.push("thing in stack");
-
+		stackTester.push("thing1");
+		stackTester.push("thing2");
+		stackTester.push("thing3");
+		
 	}
 	
-	public static void runTests(Stack stack) {
-	 	
+	public static void runTests() {
+		
 	}
 
 }
