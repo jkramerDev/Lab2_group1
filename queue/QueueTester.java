1980976b547cddf25f27f9767ccb242cf929715d
Cameron Palmer <cameron.palmer@student.fairfield.edu>
2024-02-14 16:23:42 -0500

    2

--- queue/QueueTester.java
+++ queue/QueueTester.java
@@ -8,7 +8,15 @@ public class QueueTester {
 		q.enQueue("thing 1");
 		q.enQueue("thing 2");
 		q.enQueue("thing 3");
+		q.enQueue("thing 4");
 		
+		System.out.println("Queue Head: " + q.peek());
+		q.display();
+		
+		q.deQueue();
+		
+		System.out.println("Queue Head: " + q.peek());
+		q.display();
 	}
 
 }
